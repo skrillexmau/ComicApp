a6e1411dbc941d603ef7d31f846f3e8e5e9ec6be
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-03-17 17:07:59 -0600

    Added home.routes

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HomeComponent} from './component/home.component';
+import {HomeRouting} from './home.routes';
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HomeRouting
   ]
 })
 export class HomeModule { }
