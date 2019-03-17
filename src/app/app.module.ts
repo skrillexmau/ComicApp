a9c2de986b206efac069cf9d06073e938e425c0d
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-03-17 15:03:08 -0600

    added navbar module to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {NavbarModule} from './component/navbar/navbar.module';
+import { HomeComponent } from './pages/home/component/home.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
